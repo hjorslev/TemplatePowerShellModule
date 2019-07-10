f98438d10d7eeb3dca7ce7064ae51c0d6fa6f4f6
Frederik Hjorslev <frederik@hjorslev.com>
2019-07-10 10:52:55 +0200

    pester manifest

--- Tests/ModuleValidation.Tests.ps1
+++ Tests/ModuleValidation.Tests.ps1
@@ -2,6 +2,9 @@
 $ModuleRoot = Split-Path (Resolve-Path "$($ProjectRoot)/*/*.psm1")
 $ModuleName = Split-Path $ModuleRoot -Leaf
 
+$PublicFiles = @(Get-ChildItem -Path "$($ProjectRoot)\Public\*.ps1" -ErrorAction SilentlyContinue)
+$ModuleInformation = Import-Metadata -Path "$($ProjectRoot)\$($ModuleName)\$($ModuleName).psd1"
+
 Describe "General project validation: $($ModuleName)" {
     $FileSearch = @{
         Path    = $ProjectRoot
@@ -25,5 +28,26 @@ Describe "General project validation: $($ModuleName)" {
 
     It "'$ModuleName' can import cleanly" {
         { Import-Module (Join-Path $ModuleRoot "$ModuleName.psm1") -Force } | Should -Not -Throw
+    }
+
+    Context 'Manifest' {
+        It 'Should contains RootModule' { $ModuleInformation.RootModule | Should -Not -BeNullOrEmpty }
+        It 'Should contains Author' { $ModuleInformation.Author | Should -Not -BeNullOrEmpty }
+        It 'Should contains Company Name' { $ModuleInformation.CompanyName | Should -Not -BeNullOrEmpty }
+        It 'Should contains Description' { $ModuleInformation.Description | Should -Not -BeNullOrEmpty }
+        It 'Should contains Copyright' { $ModuleInformation.Copyright | Should -Not -BeNullOrEmpty }
+        It 'Should contains License' { $ModuleInformation.PrivateData.PSData.LicenseURI | Should -Not -BeNullOrEmpty }
+        It 'Should contains a Project Link' { $ModuleInformation.PrivateData.PSData.ProjectURI | Should -Not -BeNullOrEmpty }
+        It 'Should contain Tags (For the PSGallery)' { $ModuleInformation.Tags.Count | Should -Not -BeNullOrEmpty }
+
+        It "Should have equal number of Function Exported and the Public PS1 files found ($($ExportedFunctions.Count) and $($PublicFiles.Count))" {
+            $ExportedFunctions.Count -eq $PublicFiles.Count | Should -Be $true }
+
+        It "Compare the missing function" {
+            if (-not ($ExportedFunctions.Count -eq $PublicFiles.Count)) {
+                $Commandompare = Compare-Object -ReferenceObject $ExportedFunctions -DifferenceObject $PublicFiles.BaseName
+                $Commandompare.InputObject -join ',' | Should -BeNullOrEmpty
+            }
+        }
     }
 }
