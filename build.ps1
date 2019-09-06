7af54987190d119a230f06e2d03758429ea9ad39
Frederik Hjorslev Poulsen <frederik@hjorslev.com>
2019-09-06 21:41:40 +0200

    InvokeBuild

--- build.ps1
+++
@@ -1,9 +0,0 @@
-param ($Task = 'Default')
-
-# Grab nuget bits, install modules, set build variables, start build.
-Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
-
-Set-BuildEnvironment -ErrorAction SilentlyContinue
-
-Invoke-psake -buildFile $ENV:BHProjectPath\psake.ps1 -taskList $Task -nologo
-exit ( [int]( -not $psake.build_success ) )
